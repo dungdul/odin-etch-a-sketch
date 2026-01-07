6fc429fc3eb7a106a08ec2bb402ee0c00c2804e6
dungdul <178075100+dungdul@users.noreply.github.com>
2026-01-07 21:57:54 +0900

    Let user specify grid dimension
    Add resetGrid function which prompts used for number of squares
    per side, delete old grid, then create new grid with that number
    of squares. This function will be triggered when user presses
    the reset button.

--- javacript.js
+++ javacript.js
@@ -8,12 +8,26 @@ function createGrid(gridDimension) {
         let squareDiv = document.createElement("div");
         containerDiv.append(squareDiv);
         let squareSize = CONTAINER_SIZE / gridDimension;
-        squareDiv.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`
+        squareDiv.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`;
         squareDiv.style.border = "1px solid black"; // For debugging
 
         // Make the squares change their color when mouse cursor passes over them
         squareDiv.addEventListener("mouseenter", () => {squareDiv.style.backgroundColor = "black"});
     }
 }
+
+function resetGrid() {
+    // Prompt grid's dimension from user until user gives a valid input
+    let dimension;
+    do {
+        dimension = Number(prompt("Number of squares per side (1-100)"));
+    } while (!Number.isInteger(dimension) || dimension < 1 || dimension > 100);
+
+    // Rewrite the grid
+    containerDiv.innerHTML = "";
+    createGrid(dimension);
+}
+
+document.querySelector("button").onclick = resetGrid;
 
 createGrid(16)
