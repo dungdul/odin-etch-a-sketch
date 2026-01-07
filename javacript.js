ea8c0b2396d44327328095edf8155c139d2e1eed
dungdul <178075100+dungdul@users.noreply.github.com>
2026-01-07 20:28:20 +0900

    Set up html and add createGrid function
    Now this page has a 16*16 grid. No functionality yet.

---
+++ javacript.js
@@ -0,0 +1,16 @@
+const CONTAINER_SIZE = 960;
+const containerDiv = document.querySelector(".container");
+
+containerDiv.style.cssText = `width: ${CONTAINER_SIZE}px; height: ${CONTAINER_SIZE}px`
+
+function createGrid(gridDimension) {
+    for (let i = 0; i < gridDimension ** 2; i++) {
+        let squareDiv = document.createElement("div");
+        containerDiv.append(squareDiv);
+        let squareSize = CONTAINER_SIZE / gridDimension;
+        squareDiv.style.cssText = `width: ${squareSize}px; height: ${squareSize}px;`
+        squareDiv.style.border = "1px solid black"; // For debugging
+    }
+}
+
+createGrid(16)
