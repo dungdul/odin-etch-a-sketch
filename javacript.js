9a2feabd53e1aa5c229e1dd4d26bcbb5b9369008
dungdul <178075100+dungdul@users.noreply.github.com>
2026-01-11 14:32:13 +0900

    Add randomize color functionality
    User can now click on "randomize color" button to randomize color of
    a square every time mouse cursor passes through it.

--- javacript.js
+++ javacript.js
@@ -3,17 +3,38 @@ const CONTAINER_SIZE = 600;
 const containerDiv = document.querySelector(".container");
 containerDiv.style.cssText = `width: ${CONTAINER_SIZE}px; height: ${CONTAINER_SIZE}px`
 
+// Get option buttons
+const randomColorButton = document.querySelector("#random-color");
+
 function createGrid(gridDimension) {
     for (let i = 0; i < gridDimension ** 2; i++) {
+        // Create a square and add it to the grid
         let squareDiv = document.createElement("div");
         containerDiv.append(squareDiv);
         let squareSize = CONTAINER_SIZE / gridDimension;
         squareDiv.style.cssText = `box-sizing: border-box; width: ${squareSize}px; height: ${squareSize}px;`;
         squareDiv.style.border = "1px solid black"; // For debugging
 
-        // Make the squares change their color when mouse cursor passes over them
-        squareDiv.addEventListener("mouseenter", () => {squareDiv.style.backgroundColor = "black"});
+        // Make the square change its color when mouse cursor passes over
+        squareDiv.addEventListener(
+            "mouseenter", 
+            () => {
+                if (randomColorButton.value === "false") {
+                    squareDiv.style.backgroundColor = "black";
+                } else {
+                    squareDiv.style.backgroundColor = randomizeColor();
+                }
+            }
+        );
     }
+}
+
+function randomizeColor() {
+    rgbValues = [];
+    for (let i = 0; i < 3; i++) {
+        rgbValues.push(Math.floor(Math.random() * 256));
+    }
+    return `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
 }
 
 function newGrid() {
@@ -33,6 +54,14 @@ document.querySelector("#new-grid").onclick = newGrid;
 document.querySelector("#clear").onclick = () => {
     document.querySelectorAll(".container > div").forEach((squareDiv) => {squareDiv.style.backgroundColor = ""})
 };
+randomColorButton.onclick = () => {
+    if (randomColorButton.value === "true") {
+        randomColorButton.value = "false";
+    } else {
+        randomColorButton.value = "true";
+    }
+    randomColorButton.classList.toggle("activated");
+}
 
 // Render 16*16 grid by default
 createGrid(16)
